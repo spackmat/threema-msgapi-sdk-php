f26f762bb31bebaf56fd325ea002971120e94174
Steve Pavarno <pavarnos@gmail.com>
2018-08-11 21:26:03 +1200

    add getters to KeyPair

--- src/Threema/Core/KeyPair.php
+++ src/Threema/Core/KeyPair.php
@@ -8,13 +8,25 @@ namespace Threema\Core;
 
 class KeyPair
 {
+    /** @var string */
     public $privateKey;
 
+    /** @var string */
     public $publicKey;
 
-    public function __construct($privateKey, $publicKey)
+    public function __construct(string $privateKey, string $publicKey)
     {
         $this->privateKey = $privateKey;
         $this->publicKey  = $publicKey;
+    }
+
+    public function getPrivateKey(): string
+    {
+        return $this->privateKey;
+    }
+
+    public function getPublicKey(): string
+    {
+        return $this->publicKey;
     }
 }
